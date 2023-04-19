a39d0aa37d5c45af2c34296008619935c52737a2
Daniel Ribas <ds_ribas@hotmail.com>
2023-04-19 11:29:41 -0300

    improvement: remove messages from Conversation

--- Domain/Model/Conversation.swift
+++ Domain/Model/Conversation.swift
@@ -8,17 +8,5 @@
 public struct Conversation {
   public let id: Int
   public let users: [User]
-  public let messages: [Message]
-  public var lastMessage: Message { messages.last! }
-
-  public init(id: Int, users: [User], messages: [Message]) {
-    self.id = id
-    self.users = users
-    self.messages = messages
-  }
-
-
-  public init(id: Int, users: [User], message: Message) {
-    self.init(id: id, users: users, messages: [message])
-  }
+  public let lastMessage: Message
 }
